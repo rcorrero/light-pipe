10878c9c7e9c055577475ff1be0c8730680a75b3
Richard <rcorrero@stanford.edu>
2022-07-28 18:36:02 -0500

    Change dir names

--- light_pipe/__init__.py
+++ light_pipe/__init__.py
@@ -4,7 +4,5 @@ light-pipe
 Created by Richard Correro. More to come.
 '''
 
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
 from .utils import *
 from .pipe import *
