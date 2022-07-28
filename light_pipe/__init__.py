ffdb06d342a38cf4f9d7a329d95ca59dfc3b5924
Richard <rcorrero@stanford.edu>
2022-07-28 18:13:09 -0500

    Add fix for imports

--- light_pipe/__init__.py
+++ light_pipe/__init__.py
@@ -4,5 +4,7 @@ light-pipe
 Created by Richard Correro. More to come.
 '''
 
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from .utils import *
 from .pipe import *
--- light_pipe/pipe/__init__.py
+++ light_pipe/pipe/__init__.py
@@ -1,2 +1,4 @@
-from .transformers import *
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from .storage import *
+from .transformers import *
