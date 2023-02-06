55f20d5c66ece8d6b1b3880db854136cd5ffd59b
Richard Correro <rcorrero@stanford.edu>
2023-02-06 13:24:07 -0600

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="Light-Pipe",
-    version="0.2.1",
+    version="0.2.2",
     long_description="""
     Light-Pipe is an extensible, light-weight Python framework for data pipelines that scale. It provides a set of intuitive abstractions designed to decouple pipeline implementation from the operations performed within the pipeline. It is designed to scale effortlessly, being built from the ground-up to support concurrency in all its forms, and it has zero non-standard dependencies. Light-Pipe is released under a BSD-3-Clause License.
     """,
